a183881f04c9266b0ac5140c46d96bf77455dd8d
Mohammad Ali <123582111+MohammadAliiter@users.noreply.github.com>
2023-07-07 23:01:01 +0530

    Add files via upload

---
+++ webpage.js
@@ -0,0 +1,10 @@
+document.getElementById('Btn').addEventListener('click', function() {
+    // Handle resume button click event
+    alert('Join button clicked!');
+  });
+  
+  document.getElementById('loginBtn').addEventListener('click', function() {
+    // Handle contact button click event
+    alert('Login button clicked!');
+  });
+  
